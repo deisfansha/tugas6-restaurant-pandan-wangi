90be2d6301e763ee6b759f7e990bc47918059443
deisfansha <deisfansha8082@gmail.com>
2023-09-07 09:19:16 +0700

    Initial Commit

---
+++ src/main/java/com/example/restaurant_pandan_wangi/RestaurantPandanWangiApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_pandan_wangi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantPandanWangiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantPandanWangiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restaurant_pandan_wangi/RestaurantPandanWangiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_pandan_wangi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantPandanWangiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

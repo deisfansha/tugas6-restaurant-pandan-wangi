bafe5e176f3c21f2cbbf437d7c26a620b888f014
alfarojh <alfarous.jh@gmail.com>
2023-09-07 09:57:11 +0700

    add description and git ignore

--- src/main/java/com/example/restaurant_pandan_wangi/RestaurantPandanWangiApplication.java
+++ src/main/java/com/example/restaurant_pandan_wangi/RestaurantPandanWangiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestaurantPandanWangiApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "{name propertires}");
 		SpringApplication.run(RestaurantPandanWangiApplication.class, args);
 	}
 

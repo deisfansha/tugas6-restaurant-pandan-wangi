e4a0e8925e99e4e1d9036959dde4d27189b253ff
deisfansha <deisfansha8082@gmail.com>
2023-09-08 10:45:41 +0700

    Created API create order

--- src/main/java/com/example/restaurant_pandan_wangi/controller/OrderController.java
+++ src/main/java/com/example/restaurant_pandan_wangi/controller/OrderController.java
@@ -1,8 +1,14 @@
 package com.example.restaurant_pandan_wangi.controller;
 
+import com.example.restaurant_pandan_wangi.model.ApiResponse;
+import com.example.restaurant_pandan_wangi.model.Order;
 import com.example.restaurant_pandan_wangi.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Repository
@@ -11,5 +17,14 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @PostMapping("")
+    public ResponseEntity createOrder(@RequestBody Order orderRequest){
+        boolean added = orderService.createOrder(orderRequest);
+        if (added){
+            return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse(orderService.getMessage(), orderRequest));
+        }else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse(orderService.getMessage()));
+        }
+    }
 
 }
--- src/main/java/com/example/restaurant_pandan_wangi/service/OrderService.java
+++ src/main/java/com/example/restaurant_pandan_wangi/service/OrderService.java
@@ -1,15 +1,33 @@
 package com.example.restaurant_pandan_wangi.service;
 
+import com.example.restaurant_pandan_wangi.model.Customer;
 import com.example.restaurant_pandan_wangi.model.Employee;
+import com.example.restaurant_pandan_wangi.model.Order;
+import com.example.restaurant_pandan_wangi.model.TableNumber;
+import com.example.restaurant_pandan_wangi.repository.CustomerRepository;
+import com.example.restaurant_pandan_wangi.repository.EmployeeRepository;
 import com.example.restaurant_pandan_wangi.repository.OrderRepository;
+import com.example.restaurant_pandan_wangi.repository.TableNumberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Optional;
 
 
 @Service
 public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private TableNumberRepository tableNumberRepository;
+
     private Employee current;
     private String message;
 
@@ -19,5 +37,34 @@ public class OrderService {
 
     public Employee getCurrent(){
         return current;
+    }
+
+    public boolean createOrder(Order orderRequest){
+        if (orderRequest.getCustomer() == null || orderRequest.getEmployee() == null){
+            message = "Invalid Input";
+            return false;
+        }
+
+        Optional<Employee> existingEmployee = employeeRepository.findById(orderRequest.getEmployee().getId());
+        Optional<Customer> existingCustomer = customerRepository.findById(orderRequest.getCustomer().getId());
+        Optional<TableNumber> tableNotUse = tableNumberRepository.findById(orderRequest.getTableNumber().getId());
+
+
+        if (!existingEmployee.isPresent() || !existingCustomer.isPresent() || !tableNotUse.isPresent()|| tableNotUse.get().isTableInUse()){
+            message = "Invalid Data";
+            return false;
+        }
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String created = format.format(new Timestamp(System.currentTimeMillis()));
+        tableNotUse.get().setTableInUse(true);
+        orderRequest.setTableNumber(tableNotUse.get());
+        orderRequest.setCustomer(existingCustomer.get());
+        orderRequest.setEmployee(existingEmployee.get());
+        orderRequest.setCreatedAt(Date.valueOf(created));
+        orderRepository.save(orderRequest);
+        tableNumberRepository.save(tableNotUse.get());
+        message = "Success";
+        return true;
     }
 }
